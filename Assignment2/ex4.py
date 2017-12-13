df5829f1a2d0df581db78b4349580aba81114f58
Anastasios Tzavellas <tzavellas@gmail.com>
2017-12-13 23:55:37 +0200

    Small changes Rename systems to solver Add appendix

--- Assignment2/ex4.py
+++ Assignment2/ex4.py
@@ -9,10 +9,16 @@ import numpy as np
 import systems
 
 
-A = np.array([[1., 1., 2.], [-1., 0., 2.], [3., 2., -1.]])
-B = np.array([1., -3., 8.])
+A = np.array([[1., 1., 2.],
+              [-1., 0., 2.],
+              [3., 2., -1.]])
+b = np.array([1., -3., 8.])
 ret = systems.LU(A)
 L = ret['L']
 U = ret['U']
-y = systems.ForwardSubstitution(L, B)
+print('L\n', L)
+print('\nU\n', U)
+y = systems.ForwardSubstitution(L, b)
+print('\ny=', y)
 x = systems.BackSubstitution(U, y)
+print('x=', x)
